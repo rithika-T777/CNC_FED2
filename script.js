95cc927cc1a79ce98669b93a9b0607fc31ad228f
rithika-T777 <rithikasen300@gmail.com>
2025-06-16 09:33:29 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 function toggleMenu() {
-    document.querySelector(".menu").classList.toggle("show");
+  const menu = document.getElementById("navMenu");
+  menu.classList.toggle("show");
 }
-document.addEventListener("click", function(event) {
-    const menu = document.querySelector(".menu");
-    const menuIcon = document.querySelector(".menu-icon");
-
-    if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
-        menu.classList.remove("show");
-    }
-});
-
